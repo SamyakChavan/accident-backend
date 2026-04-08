14f02fe273298d51578b3297e11097915c725677
mahichavan100106 <mahichavan275@gmail.com>
2026-04-08 14:53:00 +0530

    URL updated

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def detect_objects(image_path):
 # LLaVA (COLAB API)
 # =========================
 def get_caption(image_path):
-    url = "https://mag-viewed-correction-judicial.trycloudflare.com/predict"
+    url = "https://takes-insured-facing-copyrights.trycloudflare.com/predict"
 
     try:
         with open(image_path, 'rb') as f:
