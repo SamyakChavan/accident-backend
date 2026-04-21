408b6ddf04c1612cc74f57bb68726634f4144c98
mahichavan100106 <mahichavan275@gmail.com>
2026-04-21 19:11:40 +0530

    URL changes

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def detect_objects(image_path):
 
 # LLaVA (Colab)
 def get_caption(image_path):
-    url = "https://destinations-creator-misc-columbus.trycloudflare.com/predict"
+    url = "https://auto-springer-gabriel-stayed.trycloudflare.com/predict"
 
     try:
         with open(image_path, 'rb') as f:
