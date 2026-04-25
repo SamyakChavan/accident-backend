c3aba4a1d553aa3a3884b3a42c618e08f9ed3971
mahichavan100106 <mahichavan275@gmail.com>
2026-04-25 15:19:04 +0530

    URL updated

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def detect_objects(image_path):
 
 # LLaVA (Colab)
 def get_caption(image_path):
-    url = "https://calendar-driving-spy-resulted.trycloudflare.com/predict"
+    url = "https://substitute-unless-thought-coaching.trycloudflare.com/predict"
 
     try:
         with open(image_path, 'rb') as f:
